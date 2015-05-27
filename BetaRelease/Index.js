8dc41b4239b7f26ddfc88a45f7f6505484918b2b
StutiSulg <stutisulg@gmail.com>
2015-05-27 02:06:19 -0700

    All the goodies you need
    WOO! Thanks Darren for making this much easier! So this version doesn't
    take in the tags needed at the beginning (hard coded) but i set up
    sending the array back to the back end.
    
    Front end - we might want to CSS the crap out of the table that's placed
    ~

--- BetaRelease/Index.js
+++ BetaRelease/Index.js
@@ -2,20 +2,21 @@
 
 (function () { 
 	"use strict"; 
-	var returnToBackend = [0, 0, 0]; 
+	var returnToBackend = [0, 0]; 
 
 	window.onload = function() { 
 			
 		// Section of code that will retrieve the txt file from the server
-			var ajax = new XMLHttpRequest(); 
-			ajax.onload = create(); 
-			ajax.open("GET", "text.txt", true);
-			ajax.send(null); 
+			//var ajax = new XMLHttpRequest(); 
+			//ajax.onload = create(); 
+			//ajax.open("GET", "text.txt", true);
+			//ajax.send(null); 
 
-			//var radios = document.getElementsByName("1");  
-			//for(var i = 0; i < radios.length; i++) { 
-				//radios[i].onchange = getlevel2; 			
-			//}
+			var radios = document.getElementsByName("1");  
+			for(var i = 0; i < radios.length; i++) { 
+				radios[i].onchange = getlevel2; 			
+			}
+
 		};
 
 	function create() { 
@@ -41,12 +42,34 @@
 	function getlevel2() { 
 			//var math = ["Algebra", "Calculus", "Statistics"]; 
 			// add data to array
-					
-			returnToBackend[0] = this.value;
 
+			returnToBackend[0] = this.value;			 
+			//document.getElementById("second").style.visibility = 'visible'; 
 
-			 
-			document.getElementById("second").style.visibility = 'visible'; 
+			// Grab correct second level tabs
+			
+			var radios = document.getElementsByName("2");	// second level so Name = 2  
+			for(var i = 0; i < radios.length; i++) { 
+				if(radios[i].getAttribute("class") == this.value){
+					radios[i].onchange = getlevel3; 
+				}
+							
+			}
+	}
+
+	function getlevel3() { 
+		returnToBackend[1] = this.value; 
+		//document.getElementById("third").style.visibility = 'visible'; 
+
+		//var radios = document.getElementsByName("3");	// second level so Name = 2  
+			//for(var i = 0; i < radios.length; i++) { 
+				//if(radios[i].getAttribute("class") == this.value){
+					//radios[i].onchange = getlevel3;
+					//returnToBackend[2] = radios[i].getAttribute("class");
+					 
+				//}
+							
+			//}
 	}
 
 	function createRadioElement(array, title)
@@ -61,6 +84,14 @@
 	    radioFragment.innerHTML = radioHtml;
 
 	    return radioFragment.firstChild;
+	}
+
+	function initBackEnd() { 
+		var submitTag = document.getElementById("content"); 
+		var arrayRepresentation = document.createElement('div'); 
+		arrayRepresentation.innerHTML = "$submitBack = [" + returnToBackend[0] + "," + returnToBackend[1] + "]"; 
+
+		submitTag.appendChild(arrayRepresentation.firstChild); 
 	}
 
 } ()); 
